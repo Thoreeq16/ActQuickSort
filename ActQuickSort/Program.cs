89effab862c215b42f50152cc7057f2c9fe7aa7b
Thoreeq16 <thoriqhidayatullah16@gmail.com>
2022-10-20 16:54:01 +0700

    Create class program (fixed)

--- ActQuickSort/Program.cs
+++ ActQuickSort/Program.cs
@@ -12,6 +12,9 @@ namespace ActQuickSort
         private int[] arr = new int[20];
         private int cmp_count = 0; // number of comparasion
         private int mov_count = 0; // number of data movements
+
+        // Number of elements in array
+        private int n;
         static void Main(string[] args)
         {
         }
